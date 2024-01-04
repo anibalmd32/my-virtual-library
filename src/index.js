142595a45fed618f9f017683fcbdd44d188670fd
Anibal Mendoza <anibalmd32@gmail.com>
2024-01-04 11:06:02 -0400

    linter: instalation

--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ import { PORT } from './config/constants.js'
 
 const server = http.createServer().listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
-});
+})
 
 server.on('request', (req, res) => {
   res.writeHead(200, {
-    'Content-type': 'text/plain',
-  }).end('Success');
-});
+    'Content-type': 'text/plain'
+  }).end('Success')
+})
