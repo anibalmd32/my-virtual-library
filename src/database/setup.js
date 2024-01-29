4f449a73b4b0d09aac466867fdfc5d8ad48c1022
Anibal Mendoza <anibalmd32@gmail.com>
2024-01-29 11:07:16 -0400

    database: fix database create

--- src/database/setup.js
+++ src/database/setup.js
@@ -4,7 +4,7 @@ import { DB_OBJ_SETUP } from '../config/database.js'
 const connection = await mysql.createConnection(DB_OBJ_SETUP)
 
 try {
-  await connection.execute('CREATE DATABASE library_db')
+  await connection.execute('CREATE DATABASE IF NOT EXISTS library_db')
   console.info('Database library_db has been created')
 } catch (error) {
   console.error('An error ocurred during database creation:', error)
