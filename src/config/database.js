5a8a0437fb8586f8666569b5f88774c08b36b508
Anibal Mendoza <anibalmd32@gmail.com>
2024-03-05 18:28:42 -0500

    start db without password

--- src/config/database.js
+++ src/config/database.js
@@ -1,8 +1,7 @@
-const DB_URI = process.env.DB_URI || 'mysql://root:d3b324@localhost:3306/library_db'
+const DB_URI = process.env.DB_URI || 'mysql://root@localhost:3306/library_db'
 
 const DB_OBJ_SETUP = {
   user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASS || 'd3b324',
   host: process.env.DB_HOST || 'localhost',
   port: process.env.DB_PORT || 3306
 }
